d95f8bad274af3213d10d13a7473bbe2b50a8421
lucaseabsouza <lucaseabsouza@gmail.com>
2024-02-04 20:07:53 -0300

    Update Vercel

---
+++ js/form.js
@@ -0,0 +1,36 @@
+/*function openWhatsapp() {
+    var iname = document.getElementById("name").value;
+    var idate = document.getElementById("date").value;
+    var ichopp_pilsen_50L = document.getElementById("chopp_pilsen_50L").value;
+    var ichopp_pilsen_30L = document.getElementById("chopp_pilsen_30L").value;
+    var ichopp_amber_30L = document.getElementById("chopp_amber_30L").value;
+    var itel = document.getElementById("tel").value;
+    var url = "https://wa.me//5531995236051?text=" //
+        + "Nome" + iname
+        + "%0a"
+        + "Data desejda" + idate
+        + "%0a"
+        + "Chopp Pilsen 50L" + ichopp_pilsen_50L
+        + "Chopp Pilsen 30L" + ichopp_pilsen_30L
+        + "Chopp Amber 30L" + ichopp_amber_30L
+        + "Whatsapp" + itel
+    window.open(url, '_blank').focus();
+}
+*/
+
+/*
+const form = new FormData();
+    form.append('Nome: ', 'name');
+    form.append('Data: ', "date");
+    form.append('Pilsen 50L: ', 'chopp-pilsen-50L');
+    form.append('Pilsen 30L: ', 'chopp-pilsen-30L');
+    form.append('Amber 30L: ', 'chopp-amber-30L');
+    fetch('https://wa.me//5531995236051?', {
+        method: 'POST',
+        body: form
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
+*/
