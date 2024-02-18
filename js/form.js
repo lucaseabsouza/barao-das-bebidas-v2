19f560ba162feb541a581a02bbfa06b2d10b46f8
lucaseabsouza <lucaseabsouza@gmail.com>
2024-02-18 13:54:02 -0300

    Update in form
    Add function that send the inputs to whatsapp

--- js/form.js
+++ js/form.js
@@ -1,36 +1,15 @@
-/*function openWhatsapp() {
-    var iname = document.getElementById("name").value;
-    var idate = document.getElementById("date").value;
-    var ichopp_pilsen_50L = document.getElementById("chopp_pilsen_50L").value;
-    var ichopp_pilsen_30L = document.getElementById("chopp_pilsen_30L").value;
-    var ichopp_amber_30L = document.getElementById("chopp_amber_30L").value;
-    var itel = document.getElementById("tel").value;
-    var url = "https://wa.me//5531995236051?text=" //
-        + "Nome" + iname
-        + "%0a"
-        + "Data desejda" + idate
-        + "%0a"
-        + "Chopp Pilsen 50L" + ichopp_pilsen_50L
-        + "Chopp Pilsen 30L" + ichopp_pilsen_30L
-        + "Chopp Amber 30L" + ichopp_amber_30L
-        + "Whatsapp" + itel
-    window.open(url, '_blank').focus();
-}
-*/
-
-/*
-const form = new FormData();
-    form.append('Nome: ', 'name');
-    form.append('Data: ', "date");
-    form.append('Pilsen 50L: ', 'chopp-pilsen-50L');
-    form.append('Pilsen 30L: ', 'chopp-pilsen-30L');
-    form.append('Amber 30L: ', 'chopp-amber-30L');
-    fetch('https://wa.me//5531995236051?', {
-        method: 'POST',
-        body: form
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    });
-*/
+document.getElementById("whatsappForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var nome = document.getElementById("nome").value;
+    var idate = document.getElementById("idate").value;
+    var chopp_pilsen_50L= document.getElementById("chopp_pilsen_50L").value;
+    var chopp_pilsen_30L= document.getElementById("chopp_pilsen_30L").value;
+    var chopp_amber_30L= document.getElementById("chopp_amber_30L").value;
+    var urlWhatsApp = "https://wa.me/5531995236051/?text="
+    + "Nome: " + nome +"%0A"
+    + "Data desejada: "+idate+"%0A"
+    + "Chopp Pilsen 50L: " + chopp_pilsen_50L + "%0A"
+    + "Chopp Pilsen 30L: " + chopp_pilsen_30L + "%0A"
+    + "Chopp Amber 30L: " + chopp_amber_30L + "%0A";
+    window.open(urlWhatsApp);
+  });
