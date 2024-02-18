0c3d41924815dd7869316f5863b4e4f8d08a6d13
lucaseabsouza <lucaseabsouza@gmail.com>
2024-02-18 14:11:00 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -6,10 +6,10 @@ document.getElementById("whatsappForm").addEventListener("submit", function(even
     var chopp_pilsen_30L= document.getElementById("chopp_pilsen_30L").value;
     var chopp_amber_30L= document.getElementById("chopp_amber_30L").value;
     var urlWhatsApp = "https://wa.me/5531995236051/?text="
-    + "Nome: " + nome +"%0A"
+    +"Olá meu é "+ nome +" e estou interessado em alugar barris de chopp na quantidade e na data abaixo, aguardo retono."+"%0A"
     + "Data desejada: "+idate+"%0A"
     + "Chopp Pilsen 50L: " + chopp_pilsen_50L + "%0A"
     + "Chopp Pilsen 30L: " + chopp_pilsen_30L + "%0A"
-    + "Chopp Amber 30L: " + chopp_amber_30L + "%0A";
+    + "Chopp Amber 30L: " + chopp_amber_30L;
     window.open(urlWhatsApp);
   });
